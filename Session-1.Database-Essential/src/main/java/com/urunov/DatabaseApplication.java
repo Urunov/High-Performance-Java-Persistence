cb026d93dc406463decc681e624b40d6d5b7398c
apple <hamdamboy.urunov@gmail.com>
2020-09-12 22:23:31 +0900

    Essential Database ORM Mapping

---
+++ Session-1.Database-Essential/src/main/java/com/urunov/DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.urunov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseApplication.class, args);
+    }
+
+}
