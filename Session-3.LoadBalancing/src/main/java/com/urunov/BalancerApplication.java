43f81e299afe67374514f11c4dc06d42c32661fb
Hamdamboy Urunov <hamdamboy@Hamdamboys-MacBook-Pro.local>
2020-10-05 18:35:21 +0900

    LoadBalancing Coding

---
+++ Session-3.LoadBalancing/src/main/java/com/urunov/BalancerApplication.java
@@ -0,0 +1,13 @@
+package com.urunov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalancerApplication.class, args);
+    }
+
+}
